d83607a846bd511bf7227e9e7e66b4caff9cf0ec
Lateef Idris <lateef.idris17@gmail.com>
2024-02-13 22:39:31 +0000

    Added Update Directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -50,4 +50,20 @@ class DirectorsController < ApplicationController
 
   end
 
+  def update
+    dir_id = params.fetch("the_id")
+
+    matching_records = Director.where({ :id => dir_id})
+    the_director = matching_records.at(0)
+
+    the_director.name = params.fetch("the_name")
+    the_director.dob = params.fetch("the_dob")
+    the_director.bio = params.fetch("the_bio")
+    the_director.image = params.fetch("the_image")
+
+    the_director.save
+
+    redirect_to("/directors/#{dir_id}")
+  end
+
 end
